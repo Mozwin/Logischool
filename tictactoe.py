9aec00c08fe09b0954ef20f98034f7124c02528b
Clara Mozes <102215958+Mozwin@users.noreply.github.com>
2024-05-14 11:55:47 +0200

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -28,5 +28,20 @@ while game:
             print("Nem érvényes mezőt választottál")
     tabla[sor - 1][oszlop -1] = "x" if player1 else "o"
     # nyertem, vesztettem, döntetlen
-
+    for i in range(3):
+        if game and (tabla[i][0] != " " and tabla[i][0] == tabla[i][1] and tabla[i][1] == tabla[i][2] or tabla[0][i] != " " and tabla[i][0] == tabla[1][i] and tabla[1][i] == tabla[2][i]):
+            game = False
+            print(f"{'1.' if player1 else '2.'} jatekos nyert")
+        # keresztbe nyeres
+    if game and (tabla[0][0] != " " and tabla[0][0] == tabla[1][1] and tabla[1][1] == tabla[2][2] or tabla[0][2] != " " and tabla[0][2] == tabla[1][1] and tabla[1][1] == tabla[2][0]):
+        game = False
+        print(f"{'1.' if player1 else '2.'} jatekos nyert")
+        # döntetlen
+    if " " not in tabla[1] and " " not in tabla[1] and " " not in tabla[2]:
+        game = False
+        print("Döntetlen")
     # 2. jöjjön az elso utan
+    if game:
+        player1 = not player1
+print()
+# tablatörlés plusz feladat
